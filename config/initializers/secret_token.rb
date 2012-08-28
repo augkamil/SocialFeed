b1a69718bde609e03d23afac12650022e7eb5088
winktechnologies <oym@poczta.onet.pl>
2012-08-29 01:10:12 +0200

    Social Feed app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialFeed::Application.config.secret_token = '9bcec29e9c4c3438d681d31b035f88e7c3d429bdc80fa7c08b33ca436dae7ef58d7b6824c502896053b76bfe6c3e31ebb7b29b6eb8477c799a810bbb81ded747'
